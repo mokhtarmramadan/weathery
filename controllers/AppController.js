77d019f5b0ca1516a3de489f6be9936e270237b1
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-08-10 13:11:32 +0300

    Added stats APi to controller APP

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,11 +1,20 @@
 import { redisConnector } from '../utils/redis';
+import { mongodbConnector } from '../utils/db';
 
 
 class AppController {
 
   static async getStatus(req, res) {
     const redisClient = await redisConnector();
-    return res.status(200).json({'redis':redisClient.isAlive()});
+    const dbClient = await mongodbConnector(); 
+    return res.status(200).json({'redis':redisClient.isAlive(), 'mongodb':dbClient.isAlive()});
+  }
+
+  static async getStats(req, res) {
+    const dbClient = await mongodbConnector();
+    const usersCount = await dbClient.nbUsers();
+    const plansCount = await dbClient.nbPlans();
+    return res.status(200).json({'users':usersCount, 'plans':plansCount});
   }
 }
 
